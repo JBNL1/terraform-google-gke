07adf87b6a5c909b021fdd1fac449fdff09716eb
JOHN OSHIKOYA <72477831+john24rel@users.noreply.github.com>
2021-05-28 01:34:41 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 data "google_container_engine_versions" "cluster_version" {
   location       = "${var.google_region}"
-  version_prefix = "${var.cluster_version}"
   project        = "${var.google_project_id}"
+  version_prefix = "${var.cluster_version}"
+  
 }
 output "cluster_version" {
   value = "${data.google_container_engine_versions.cluster_version.latest_node_version}"
@@ -13,15 +14,30 @@ provider "google" {
 }
 
 resource "google_container_cluster" "create" {
+  min_master_version = "${data.google_container_engine_versions.cluster_version.latest_node_version}"
+  node_version       = "${data.google_container_engine_versions.cluster_version.latest_node_version}"
   name               = "${var.cluster_name}"
   network            = "${var.cluster_network}"
   subnetwork         = "${var.subnetwork}"
   location           = "${var.google_region}"
-  min_master_version = "${data.google_container_engine_versions.cluster_version.latest_node_version}"
-  initial_node_count = "${var.cluster_node_count}"
   project            = "${var.google_project_id}"
-
-  node_config {
-    machine_type = "${var.machine_type}"
+  
+ node_pool {
+    name               = "${var.node_pool}"
+    version            = "${var.cluster_version}"
+    initial_node_count = "${var.cluster_node_count}"
+    management {
+    auto_repair     = "${var.auto_repair}"
+    auto_upgrade    = "${var.auto_upgrade}"
+    }
+ node_config {
+    image_type       = "${var.image_type}"
+    disk_size_gb     = "${var.disk_size_in_gb}"
+    preemptible      = "${var.preemptible_nodes}"
+    machine_type     = "${var.machine_type}"
+    labels           = "${var.labels}"
+     
+ 
+    }
   }
 }
