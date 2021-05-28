1f1e7294d2d9d1e58f2724b867e8b80342a96f2c
JOHN OSHIKOYA <72477831+john24rel@users.noreply.github.com>
2021-05-28 01:34:12 -0500

    Update module.tf

--- deployment/example/module.tf
+++ deployment/example/module.tf
@@ -1,9 +1,17 @@
 module "gke_cluster" {
-    source  = "fuchicorp/gke/google"
+    source  = "../../"
     cluster_name = "fuchicorp-cluster"
-    google_region = "us-central1"
-    google_project_id = "fsadykov-project"
-    cluster_node_count = "2"
-    cluster_version = "1.15"
-    google_credentials = "./fuchicorp-service-account.json" # service account 
+    google_region = "us-central1-c"
+    google_project_id =  "flash-sol-311601"
+    cluster_node_count = "1"
+    cluster_version = "1.18"
+    google_credentials = "./fuchicorp-service-account.json" # service account
+    image_type        = "COS"
+    disk_size_in_gb   = "10"
+    machine_type      = "n1-standard-2"
+    auto_repair     = true
+    auto_upgrade    = false
+    labels            = {
+        "label" = "fuchicorp-project"
+    }
 }
